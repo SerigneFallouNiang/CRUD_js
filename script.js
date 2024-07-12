24a7b3993f8c477a57986060c121676c7f238aaa
SerigneFallouNiang <fallouniang776@gmail.com>
2024-07-12 13:00:54 +0000

    ajout et affichage des données

---
+++ script.js
@@ -0,0 +1,87 @@
+      // script.js
+
+// Tableau pour stocker les idées
+let ideas = [];
+
+// Sélectionner les éléments du DOM
+const form = document.getElementById('ideaForm');
+const titleInput = document.getElementById('title');
+const categoryInput = document.getElementById('category');
+const descriptionInput = document.getElementById('description');
+const messagesDiv = document.getElementById('messages');
+const ideasList = document.getElementById('ideasList');
+
+// Ajouter un écouteur d'événement pour la soumission du formulaire
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+      // Valider les données du formulaire
+      const title = titleInput.value.trim();
+      const category = categoryInput.value;
+      const description = descriptionInput.value.trim();
+
+      if (title === '' || description === '') {
+        showMessage('Tous les champs sont obligatoires', 'error');
+        return;
+    }
+
+     // Ajouter l'idée au tableau
+     const idea = {
+        id: Date.now(),
+        title,
+        category,
+        description,
+        approved: false
+    };
+
+
+    // le premier variable c'est celle du tableau vide en dessus et le deuxieme c'est le stock des information de l'utilisateur
+    ideas.push(idea);
+
+    // Réinitialiser le formulaire
+    form.reset();
+
+
+    // Afficher un message de succès
+    showMessage('Idée ajoutée avec succès', 'success');
+
+    // Mettre à jour la liste des idées
+    updateIdeasList();
+    });
+    
+
+    // Fonction pour afficher un message
+function showMessage(message, type) {
+    messagesDiv.textContent = message;
+    messagesDiv.className = type;
+    messagesDiv.style.display = 'block';
+    setTimeout(() => {
+        messagesDiv.style.display = 'none';
+    }, 2000);
+}
+
+// Fonction pour mettre à jour la liste des idées
+function updateIdeasList() {
+    ideasList.innerHTML = '';
+
+    ideas.forEach(idea => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+          <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">${idea.title}</h5>
+        <h6>${idea.category}</h6>
+        <p class="card-text">${idea.description}</p>
+        <div class="action d-flex">
+          <button type="button" class="btn btn-success" onclick="approveIdea(${idea.id})">Approuver</button>
+          <button type="button" class="btn btn-danger" onclick="disapproveIdea(${idea.id})">Désapprouver</button>
+          <i class="corbeille fa-solid fa-trash fa-2xl ms-auto" style="color: #c21e1e;" onclick="deleteIdea(${idea.id})"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+        `;
+        ideasList.appendChild(div);
+    });
+}
